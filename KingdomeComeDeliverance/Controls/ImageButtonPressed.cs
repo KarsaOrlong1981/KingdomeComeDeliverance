4c994f582313248d9f1b2d2e96bafc65678016fa
Joerg Thomas <j.thomas@peaknx.com>
2023-12-30 21:04:27 +0100

    refact code

--- KingdomeComeDeliverance/Controls/ImageButtonPressed.cs
+++ KingdomeComeDeliverance/Controls/ImageButtonPressed.cs
@@ -1,19 +1,3 @@
-//-----------------------------------------------------------------------------------------------------------------
-// <copyright file="ImageButtonPressed.cs" company="PEAKnx">
-// The copyright to the computer program(s) herein is the property of PEAKnx.
-//
-// All information and materials contained herein are owned by PEAKnx and 
-// protected by German and international copyright laws.
-// The program(s) may be used and/or copied only with the written permission
-// of PEAKnx or in accordance with the terms and conditions
-// stipulated in the agreement/contract under which the program(s)
-// have been supplied.
-//
-// Copyright (c) 2017 PEAKnx.  All rights reserved.
-// </copyright>
-//-----------------------------------------------------------------------------------------------------------------
-
-
 using CommunityToolkit.Mvvm.Input;
 using System.Windows.Input;
 
